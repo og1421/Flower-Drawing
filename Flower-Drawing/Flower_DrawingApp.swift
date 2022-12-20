634bfbbdbfb3d8d51bc30fb40af6d7dc1041dc12
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:36:09 -0300

    Initial Commit

---
+++ Flower-Drawing/Flower_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flower_DrawingApp.swift
+//  Flower-Drawing
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Flower_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
